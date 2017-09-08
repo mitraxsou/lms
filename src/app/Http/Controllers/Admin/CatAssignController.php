ed697c1db6a2d0801842e9724975b97e29a4fde8
rahul sohal <rahul@iandwe.in>
2017-09-08 11:18:58 +0530

    nothing as such

---
+++ src/app/Http/Controllers/Admin/CatAssignController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CatAssignController extends Controller
+{
+    public function show()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function destroy()
+    {
+    	
+    }
+}
